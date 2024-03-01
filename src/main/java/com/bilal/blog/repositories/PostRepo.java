8321cab926b0c7ecea10c7eea1027c9fd8f37bbd
Bilal <bilalbhutto121@gmail.com>
2024-03-01 16:48:28 +0500

    searching done

--- src/main/java/com/bilal/blog/repositories/PostRepo.java
+++ src/main/java/com/bilal/blog/repositories/PostRepo.java
@@ -4,6 +4,8 @@ import com.bilal.blog.entities.Category;
 import com.bilal.blog.entities.Post;
 import com.bilal.blog.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -12,4 +14,7 @@ public interface PostRepo extends JpaRepository<Post, Integer> {
     List<Post> findByUser(User user);
     List<Post> findByCategory(Category category);
 
+//    List<Post> findByTitleContaining(String title);
+    @Query("select p from Post p where p.title like :key")
+    List<Post> searchByTitle(@Param("key") String title);
 }
